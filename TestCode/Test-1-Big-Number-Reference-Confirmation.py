e0c37bb052b6030f80603e536fcbe87cb7611b69
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-18 19:13:53 +0900

    Create Test-1-Big-Number-Reference-Confirmation.py

---
+++ TestCode/Test-1-Big-Number-Reference-Confirmation.py
@@ -0,0 +1,12 @@
+import sys
+sys.set_int_max_str_digits(2147483647)
+
+a = 2**67
+b = 2**68
+
+print((a/2) * 2)
+print("\n" * 3)
+print((b/2) * 2)
+print("\n" * 3)
+print((b-2/2) * 2)
+print("\n" * 3)
