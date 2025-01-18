606b80498787d1bd642e3718ff808bfc0069c1d0
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-18 19:19:33 +0900

    Update Test-1-Big-Number-Reference-Confirmation.py

--- TestCode/Test-1-Big-Number-Reference-Confirmation.py
+++ TestCode/Test-1-Big-Number-Reference-Confirmation.py
@@ -1,21 +1,30 @@
 import sys
 sys.set_int_max_str_digits(2147483647)
 
-a = 2**67
-b = 2**68
+# a = 2**67
+# b = 2**68
 
-print((a/2) * 2)
-print("\n" * 3)
-print((b/2) * 2)
-print("\n" * 3)
-print((b-2/2) * 2)
-print("\n" * 3)
+# print((a/2) * 2)
+# print("\n" * 3)
+# print((b/2) * 2)
+# print("\n" * 3)
+# print((b-2/2) * 2)
+# print("\n" * 3)
 
-print("==============")
+# print("==============")
 
-print((2**30/2) * 2)
-print("\n" * 3)
-print((2**31/2) * 2)
-print("\n" * 3)
-print((2**32/2) * 2)
-print("\n" * 3)
+# print((2**30/2) * 2)
+# print("\n" * 3)
+# print((2**31/2) * 2)
+# print("\n" * 3)
+# print((2**32/2) * 2)
+# print("\n" * 3)
+
+# print("==============")
+
+for i in range(1, 70):
+  ex = ((2**i)/2) * 2
+  print(f'{i}: {ex}')
+  if "e" in str(ex):
+    print("==========")
+    print((((2**i) - 2)/2) * 2)
