dd1073a91440dfd2d138bf962184aeec696e96f3
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-18 15:46:06 +0900

    Update Test-1-ExponentialNotaion-To-StandardNotation.py

--- TestCode/Test-1-ExponentialNotaion-To-StandardNotation.py
+++ TestCode/Test-1-ExponentialNotaion-To-StandardNotation.py
@@ -2,7 +2,7 @@ import sys
 sys.set_int_max_str_digits(2147483647)
 
 def ExponentialNotaion_To_StandardNotation(temp):
-  return int(str(temp)[0] + str(temp)[str(temp).find("."):(str(temp).find("e") + 1)] + ("0" * (int(str(temp)[(str(temp).find("+")):(len(str(temp)) + 1)]) - len(str(temp)[str(temp).find("."):(str(temp).find("e") + 1)]))))
+  return int(str(temp)[0] + str(temp)[(str(temp).find(".") + 1):(str(temp).find("e") + 1)] + ("0" * (int(str(temp)[((str(temp).find("+")) + 1):(len(str(temp)) + 1)]) - len(str(temp)[(str(temp).find(".") + 1):(str(temp).find("e") + 1)]))))
 
 print(ExponentialNotaion_To_StandardNotation("7.649670160387128e+16"))
 print(ExponentialNotaion_To_StandardNotation(7.649670160387128e+16))
