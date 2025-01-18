6bf229d4bbdf57a35bf681099ec81586cb81acd2
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-18 19:15:34 +0900

    Update Test-1-Big-Number-Reference-Confirmation.py

--- TestCode/Test-1-Big-Number-Reference-Confirmation.py
+++ TestCode/Test-1-Big-Number-Reference-Confirmation.py
@@ -10,3 +10,12 @@ print((b/2) * 2)
 print("\n" * 3)
 print((b-2/2) * 2)
 print("\n" * 3)
+
+print("==============")
+
+print((2**30/2) * 2)
+print("\n" * 3)
+print((2**31/2) * 2)
+print("\n" * 3)
+print((2**32/2) * 2)
+print("\n" * 3)
