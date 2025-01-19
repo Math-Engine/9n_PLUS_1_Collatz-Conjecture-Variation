2c7790eda26543a70e71e97d0e4e6bbf3b1a833b
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-19 21:39:34 +0900

    Update Print-9n_PLUS_1-Sequence.py

--- Print-9n_PLUS_1-Sequence.py
+++ Print-9n_PLUS_1-Sequence.py
@@ -3,6 +3,16 @@ import os
 
 sys.set_int_max_str_digits(2147483647)
 
+def dividing_2(n):
+  if (len(str(n)) < 16):
+    return n//2
+  n = str(n)
+  r = 0
+  for i in range(len(n) - 1):
+    r = r + int(str(int(n[i] + "0")//2) + ("0" * (len(n) - 2 - i)))
+  r = r + int(n[-1])//2
+  return r
+
 num = int(sys.argv[1])
 sequence_length = int(sys.argv[2])
 
@@ -14,10 +24,10 @@ is_loop = False
 is_end = False
 for i in range(sequence_length - 1):
   print(temp, end=" - ")
-  if (temp%2 == 1):
+  if (int(temp[-1])%2 == 1):
     temp = (temp * 9) + 1
   else:
-    temp = temp/2
+    temp = dividing_2(temp)
   if temp in Hailstone_Num:
     is_loop = True
     Hailstone_Num.append(temp)
