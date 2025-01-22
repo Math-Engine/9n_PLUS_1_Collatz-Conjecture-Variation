1d39434c63f6b8909486126302d5ab968dc7a704
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-22 20:17:34 +0900

    9n_PLUS_1.py에서 dividing_2(n) 함수 최적화

--- 9n_PLUS_1.py
+++ 9n_PLUS_1.py
@@ -6,12 +6,7 @@ sys.set_int_max_str_digits(2147483647)
 def dividing_2(n):
   if (len(str(n)) < 16):
     return n//2
-  n = str(n)
-  r = 0
-  for i in range(len(n) - 1):
-    r = r + int(str(int(n[i] + "0")//2) + ("0" * (len(n) - 2 - i)))
-  r = r + int(n[-1])//2
-  return r
+  return int(str((n * 5))[0:len(str(n)) - 1])
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
