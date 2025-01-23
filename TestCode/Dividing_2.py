bb46b08046d10a2cf3293c8a24aa16f26535a6ac
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-23 23:37:23 +0900

    TestCode/Dividing_2.py 개선된 코드로 수정

--- TestCode/Dividing_2.py
+++ TestCode/Dividing_2.py
@@ -3,12 +3,7 @@
 import sys
 sys.set_int_max_str_digits(2147483647)
 
-def algorithm_1(n):
+def dividing_2(n):
   if (len(str(n)) < 16):
     return n//2
-  n = str(n)
-  r = 0
-  for i in range(len(n) - 1):
-    r = r + int(str(int(n[i] + "0")//2) + ("0" * (len(n) - 2 - i)))
-  r = r + int(n[-1])//2
-  return r
+  return int(str((n * 5))[0:len(str(n * 5)) - 1])
